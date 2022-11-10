e940b0f6fd38808f86993d664c02aaff6be1a4c1
Brian Barquesi <barquesi.tech@gmail.com>
2022-11-09 23:09:49 -0300

    sorted LoginPage args

--- lib/login/presentation/login_page_main.dart
+++ lib/login/presentation/login_page_main.dart
@@ -1,140 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sistema_ies/core/domain/ies_system.dart';
-import 'package:sistema_ies/core/presentation/widgets/fields.dart';
 import 'package:sistema_ies/login/domain/login.dart';
+import 'package:sistema_ies/login/presentation/login_page_main.dart';
+import 'package:sistema_ies/login/presentation/password_reset_sent.dart';
+import 'package:sistema_ies/login/presentation/recovery_pass_page.dart';
 
-class LoginPageMain extends StatelessWidget {
-  const LoginPageMain({
-    Key? key,
-    required GlobalKey<FormState> formKey,
-    required TextEditingController emailTextController,
-    required TextEditingController passwordTextController,
-    required LoginState loginStatesProvider,
-  })  : _formKey = formKey,
-        _emailTextController = emailTextController,
-        _passwordTextController = passwordTextController,
-        _loginStatesProvider = loginStatesProvider,
-        super(key: key);
-
-  final GlobalKey<FormState> _formKey;
-  final TextEditingController _emailTextController;
-  final TextEditingController _passwordTextController;
-  final LoginState _loginStatesProvider;
+class LoginPage extends ConsumerWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Ver',
-                style: Theme.of(context).textTheme.headline1,
-              ),
-              Text('App', style: Theme.of(context).textTheme.headline2)
-            ],
-          ),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 198, 198, 198)),
-      body: Container(
-        decoration:
-            const BoxDecoration(color: Color.fromARGB(255, 255, 255, 255)),
-        child: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 420),
-            width: MediaQuery.of(context).size.width / 0.5,
-            child: Form(
-                key: _formKey,
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 40, vertical: 150),
-                  child: Wrap(
-                    crossAxisAlignment: WrapCrossAlignment.center,
-                    alignment: WrapAlignment.center,
-                    //mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      fieldEmailDNI(
-                          _emailTextController, "Email o DNI", false, context),
-                      const SizedBox(height: 10),
-                      fieldPassword(
-                          _passwordTextController, "Contraseña", true, context),
-                      const SizedBox(height: 60),
-                      Container(
-                        width: MediaQuery.of(context).size.width / 0.5,
-                        height: 50,
-                        decoration: const BoxDecoration(
-                            color: Color.fromARGB(255, 36, 110, 221),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: TextButton(
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              await IESSystem().loginUseCase.signIn(
-                                  _emailTextController.text.trim(),
-                                  _passwordTextController.text.trim());
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content:
-                                          Text("Completa todos los campos")));
-                            }
-                            if (_loginStatesProvider.stateName ==
-                                LoginStateName.failure) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                      backgroundColor:
-                                          Theme.of(context).colorScheme.error,
-                                      content: const Text(
-                                          "Usuario o contraseñas incorrecta")));
-                            } else if (_loginStatesProvider.stateName ==
-                                LoginStateName.emailNotVerifiedFailure) {
-                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                  backgroundColor:
-                                      Theme.of(context).colorScheme.primary,
-                                  content: const Text(
-                                      "Su email no ha sido verificado aún. Revise si casilla de correos por favor")));
-                            }
-                          },
-                          child: const Text(
-                            'Iniciar Sesión',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text("¿No tienes cuenta?"),
-                          TextButton(
-                            onPressed: () async {
-                              IESSystem().startRegisteringNewUser();
-                            },
-                            child: Text(
-                              'Registrate',
-                              style: TextStyle(
-                                  color: Theme.of(context).colorScheme.primary),
-                            ),
-                          ),
-                        ],
-                      ),
-                      TextButton(
-                        onPressed: () async {
-                          IESSystem().loginUseCase.startRecoveryPass();
-                        },
-                        child: Text(
-                          '¿Olvidaste la contraseña?',
-                          style: TextStyle(
-                              color: Theme.of(context).colorScheme.secondary),
-                        ),
-                      ),
-                    ],
-                  ),
-                )),
-          ),
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _loginStatesProvider =
+        ref.watch(IESSystem().loginUseCase.stateNotifierProvider);
+    print(_loginStatesProvider.stateName.name);
+    final List<Widget> _widgetOptions = <Widget>[
+      LoginPageMain(),
+      RecoveryPassPage(),
+      passwordResetSent(context),
+      const Center(
+        child: CircularProgressIndicator(),
       ),
-    );
+    ];
+    final _elements = {
+      LoginStateName.init: 0,
+      LoginStateName.recoverypass: 1,
+      LoginStateName.passwordResetSent: 2,
+      LoginStateName.successfullySignIn: 3,
+      LoginStateName.emailNotVerifiedFailure: 5,
+      LoginStateName.verificationEmailSent: 6,
+      LoginStateName.failure: 0,
+    };
+    return _widgetOptions.elementAt(_elements[_loginStatesProvider.stateName]!);
   }
 }
