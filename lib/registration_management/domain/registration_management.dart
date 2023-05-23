205d71a263d703ac37775a9a390d30d91e31b4b7
monchoK <kevinguevara9876mail.com>
2023-05-23 13:04:46 -0300

    add enrollment registration statuses

---
+++ lib/registration_management/domain/registration_management.dart
@@ -0,0 +1,5 @@
+import 'package:sistema_ies/core/domain/entities/syllabus.dart';
+import 'package:sistema_ies/core/domain/entities/users.dart';
+import 'package:sistema_ies/core/domain/utils/operation_utils.dart';
+
+enum RegistrationManagementStateName { init }
