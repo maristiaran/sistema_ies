78eede8b72c498f42fb4fb842848f2124500de4d
monchoK <kevinguevara9876mail.com>
2023-06-09 18:20:31 -0300

    registration management changes

--- lib/registration_management/domain/registration_management.dart
+++ lib/registration_management/domain/registration_management.dart
@@ -1,6 +1,8 @@
 import 'package:sistema_ies/core/domain/entities/syllabus.dart';
 import 'package:sistema_ies/core/domain/entities/users.dart';
 import 'package:sistema_ies/core/domain/utils/operation_utils.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 enum RegistrationManagementStateName {
   init,
@@ -8,19 +10,31 @@ enum RegistrationManagementStateName {
 }
 
 class RegistrationManagementState extends OperationState {
-  const RegistrationManagementState({required stateName})
+  const RegistrationManagementState({required Enum stateName})
       : super(stateName: stateName);
 
   @override
   List<Object?> get props => [];
 }
 
-class InitRegistrationManagementState extends RegistrationManagementState {}
+class InitRegistrationManagementState extends RegistrationManagementState {
+  const InitRegistrationManagementState()
+      : super(stateName: RegistrationManagementStateName.init);
+}
 
-class FailureRegistrationManagementState extends RegistrationManagementState {}
+class FailureRegistrationManagementState extends RegistrationManagementState {
+  const FailureRegistrationManagementState()
+      : super(stateName: RegistrationManagementStateName.failure);
+}
 
 class RegistrationManagementUseCase
     extends Operation<RegistrationManagementState> {
-  RegistrationManagementUseCase(RegistrationManagementState initialState)
-      : super(initialState);
+  RegistrationManagementUseCase()
+      : super(const InitRegistrationManagementState());
 }
+
+// Proveedores de estado
+final registrationManagementUseCaseProvider =
+    Provider<RegistrationManagementUseCase>((ref) {
+  return RegistrationManagementUseCase();
+});
